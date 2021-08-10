e7cb269d3bf2dac11f46d4342ae0fedea7c224aa
Alli <allisonglittle@gmail.com>
2021-08-09 17:07:41 -0700

    Added pages.

--- exercise-ui/src/App.js
+++ exercise-ui/src/App.js
@@ -1,11 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import CreateExercisePage from './pages/CreateExercisePage';
+import EditExercisePage from './pages/EditExercisePage';
+import { useState } from 'react';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
