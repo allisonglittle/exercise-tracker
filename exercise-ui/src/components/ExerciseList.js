075de8d954981a54535eaf377a3c374233d203e9
Alli <allisonglittle@gmail.com>
2021-08-09 17:25:41 -0700

    Created components for exercise list and exercises

---
+++ exercise-ui/src/components/ExerciseList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Exercise from './Exercise';
+
+function ExerciseList( exercises ) {
+    return (
+        <>
+        </>
+    );
+}
+
+export default ExerciseList;
