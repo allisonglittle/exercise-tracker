768bcddff0ceaf4077f1d1992639c8e84950e4b9
Alli <allisonglittle@gmail.com>
2021-08-09 17:28:25 -0700

    Created table detail within Exercise.

--- exercise-ui/src/components/Exercise.js
+++ exercise-ui/src/components/Exercise.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { MdDeleteForever, MdEdit } from 'react-icons/md';
 
-function Exercise() {
+function Exercise( exercise) {
     return (
-        <>
-        </>
+        <tr>
+            <td>{exercise.name}</td>
+            <td>{exercise.reps}</td>
+            <td>{exercise.weight}</td>
+            <td>{exercise.unit}</td>
+            <td>{exercise.date}</td>
+            <td><MdEdit /></td>
+            <td><MdDeleteForever /></td>
+        </tr>
     );
 }
 
