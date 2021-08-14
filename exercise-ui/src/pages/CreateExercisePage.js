f67a81371992f9d9c44f095ac192534881827dfc
Alli <allisonglittle@gmail.com>
2021-08-13 22:14:28 -0700

    Updated style of create exercise pages

--- exercise-ui/src/pages/CreateExercisePage.js
+++ exercise-ui/src/pages/CreateExercisePage.js
@@ -31,26 +31,23 @@ function CreateExercisePage() {
     return (
         <div>
             <h1>Add exercise</h1>
-
-            <label>Name: 
-                <input type="text" value={name} onChange={e => setName(e.target.value)} />
-            </label>
-            <input
-                type="number"
-                value={reps}
-                onChange={e => setReps(e.target.value)} />
-            <input
-                type="number"
-                value={weight}
-                onChange={e => setWeight(e.target.value)} />
-            <input
-                type="text"
-                value={unit}
-                onChange={e => setUnit(e.target.value)} />
-            <input
-                type="date"
-                value={date}
-                onChange={e => setDate(e.target.value)} />
+            <div>
+                <label className="inputLabel">Name: 
+                    <input className="textInput" type="text" value={name} onChange={e => setName(e.target.value)} />
+                </label>
+                <label className="inputLabel">Reps:
+                    <input className="numericInput" type="number" value={reps} onChange={e => setReps(e.target.value)} />
+                </label>
+                <label className="inputLabel">Weight:
+                    <input className="numericInput" type="number" value={weight} onChange={e => setWeight(e.target.value)} />
+                </label>
+                <label className="inputLabel">Unit: 
+                    <input className="numericInput" type="text" value={unit} onChange={e => setUnit(e.target.value)} />
+                </label>
+                <label className="inputLabel">Date: 
+                    <input type="date" value={date} onChange={e => setDate(e.target.value)} />
+                </label>
+            </div>
             <button
                 onClick={addExercise}
             >Submit</button>
